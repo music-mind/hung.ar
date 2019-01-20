69dacb9331e94770b2d4f63fa43801dc03d8ffb8
music-mind <arriye_@hotmail.com>
2019-01-20 01:04:55 -0500

    added images and eating

--- App.js
+++ App.js
@@ -1,22 +1,150 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RNCamera, FaceDetector } from 'react-native-camera';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, Permissions } from 'expo';
+import { Button, Image } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+    hasFood: false,
+    data: {},
+    stage: 0,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  buyFood = () => {
+    fetch('https://music-mind.lib.id/food@dev/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({
+          ...this.state,
+          data: responseJson,
+          hasFood: true,
+          stage: 0,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    return;
+  }
+
+  eatFood = () => {
+    const { stage } = this.state;
+    if (stage <= 1) {
+      this.setState({
+        ...this.state,
+        stage: stage + 1
+      });
+    } else if (stage == 2) {
+      this.setState({
+        ...this.state,
+        hasFood: false
+      })
+    }
+    return;
+  }
+
+  // https://music-mind.lib.id/food@dev/
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Woooo!</Text>
-      </View>
-    );
+    const { hasCameraPermission, hasFood, data, stage } = this.state;
+
+    let centerItem = hasFood ? <Image source={{uri: data.src[stage]}} style={{ width: 600, height: 600 }} /> : <View />;
+
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1, justifyContent: 'flex-start'}} type={this.state.type}>
+            <View style={{top: 50, right: -100, position: 'absolute', backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center'}} >{centerItem}</View>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={this.buyFood}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Buy{' '}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={this.eatFood}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Eat{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
+
+AppRegistry.registerComponent('App', () => App)
